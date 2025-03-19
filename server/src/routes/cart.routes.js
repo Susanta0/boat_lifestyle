70df1e1bdf476c49ea3f1fa6076ac33b4d7aac80
Susanta0 <susanta721467@gmail.com>
2025-03-19 16:36:43 +0530

    updated backend.

--- server/src/routes/cart.routes.js
+++ server/src/routes/cart.routes.js
@@ -4,18 +4,26 @@ const cartControllers = require("../controllers/cart.controllers");
 const router = express.Router();
 
 // Get all products in the authenticated user's cart
-router.get("/products", cartControllers.getAllProductsInCart);
+router.get("/products", protect, cartControllers.getAllProductsInCart);
 
 // Add a new cart
-router.post("/", cartControllers.addCart);
+router.post("/", protect, cartControllers.addCart);
 
 // Remove a product from the cart
-router.delete("/products/:productId", cartControllers.removeProductFromCart);
+router.delete(
+  "/products/:productId",
+  protect,
+  cartControllers.removeProductFromCart
+);
 
 // Update the quantity of a product in the cart
-router.put("/products/:productId", cartControllers.updateProductQuantityInCart);
+router.put(
+  "/products/:productId",
+  protect,
+  cartControllers.updateProductQuantityInCart
+);
 
 // Delete all products from the cart
-router.delete("/", cartControllers.clearCart);
+router.delete("/", protect, cartControllers.clearCart);
 
 module.exports = router;
